f2ab2414146121e2ab1268168dba33a68fdd046a
RyuH1 <haichuan@ata.network>
2022-03-14 15:46:19 +0800

    Example for Proposals

--- tests/queries.rs
+++ tests/queries.rs
@@ -1,11 +1,11 @@
 mod structs;
 
-use crate::structs::{inputs::SinglePostVariables, AllPosts, SinglePost};
+use crate::structs::{inputs::{SinglePostVariables, SingleProposalVariables}, AllPosts, SinglePost, AllProposals};
 use gql_client::Client;
 use std::collections::HashMap;
 
 // Initialize endpoint
-const ENDPOINT: &'static str = "https://graphqlzero.almansi.me/api";
+const ENDPOINT: &'static str = "https://api.subquery.network/sq/RyuH1/fst_anydao__Unl1S";
 
 #[tokio::test]
 pub async fn fetches_one_post() {
@@ -48,4 +48,97 @@ pub async fn fetches_all_posts() {
   let data: AllPosts = client.query_unwrap::<AllPosts>(query).await.unwrap();
 
   assert!(data.posts.data.len() > 0 as usize);
+}
+
+#[tokio::test]
+pub async fn fetches_all_projects() {
+  let mut headers = HashMap::new();
+  headers.insert("content-type", "application/json");
+
+  let client = Client::new_with_headers(ENDPOINT, headers);
+
+  let query = r#"
+    query AllProjectsQuery {
+      proposals {
+        nodes {
+          id
+          project {
+            id
+            owner {
+              id
+              protocol
+            }
+            data
+            updated
+          }
+          proposal
+          start
+          end
+          author {
+            id
+            protocol
+          }
+          privacy
+          frequency
+          status
+          votes
+          pubvote
+          created
+        }
+      }
+    }
+  "#;
+
+  let data: AllProposals = client.query_unwrap::<AllProposals>(query).await.unwrap();
+
+  assert!(data.proposals.nodes.len() > 0 as usize);
+
+  println!("data: {:?}", data);
+}
+
+#[tokio::test]
+pub async fn fetches_all_projects_with_filter() {
+  let mut headers = HashMap::new();
+  headers.insert("content-type", "application/json");
+
+  let client = Client::new_with_headers(ENDPOINT, headers);
+
+  let query = r#"
+    query AllProjectsQuery($current: BigFloat!) {
+      proposals(filter: {end: {greaterThanOrEqualTo: $current}}) {
+        nodes {
+          id
+          project {
+            id
+            owner {
+              id
+              protocol
+            }
+            data
+            updated
+          }
+          proposal
+          start
+          end
+          author {
+            id
+            protocol
+          }
+          privacy
+          frequency
+          status
+          votes
+          pubvote
+          created
+        }
+      }
+    }
+  "#;
+
+  let variables = SingleProposalVariables { current: "1646841600000".to_string() };
+  let data: AllProposals = client.query_with_vars_unwrap::<AllProposals, SingleProposalVariables>(query, variables).await.unwrap();
+
+  assert!(data.proposals.nodes.len() > 0 as usize);
+
+  println!("data: {:?}", data);
 }
--- tests/structs.rs
+++ tests/structs.rs
@@ -1,4 +1,4 @@
-use serde::Deserialize;
+use serde::{Deserialize, de::Error};
 
 #[derive(Deserialize, Debug)]
 pub struct NodeList<T> {
@@ -27,4 +27,93 @@ pub mod inputs {
   pub struct SinglePostVariables {
     pub id: u32,
   }
+
+  #[derive(Serialize, Debug)]
+  pub struct SingleProposalVariables {
+    pub current: String,
+  }
+}
+
+//////////////////////////////////
+/// AnyDAO
+
+#[derive(Deserialize, Debug)]
+pub enum Protocol {
+  Substrate,
+  Solidity
+}
+
+#[derive(Deserialize, Debug)]
+pub enum Privacy {
+  Opaque,
+  Private,
+  Public,
+  Mixed
+}
+
+#[derive(Deserialize, Debug)]
+pub enum Status {
+  Pending,
+  Ongoing,
+  Closed
+}
+
+#[derive(Deserialize, Debug)]
+pub struct CrossChainAccount {
+  pub id: String,
+  pub protocol: Protocol
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Project {
+  pub id: String,
+  pub owner: CrossChainAccount,
+  pub data: String,
+  pub updated: u32
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Proposal {
+  pub id: String,
+  pub project: Project,
+  pub proposal: u32,
+  #[serde(deserialize_with = "deserialize_u64")]
+  pub start: u64,
+  #[serde(deserialize_with = "deserialize_u64")]
+  pub end: u64,
+  pub author: CrossChainAccount,
+  pub privacy: Privacy,
+  pub frequency: Option<String>,
+  pub status: Status,
+  #[serde(deserialize_with = "deserialize_vec")]
+  pub votes: Vec<u64>,
+  pub pubvote: Option<String>,
+  pub created: u32
+}
+
+#[derive(Deserialize, Debug)]
+pub struct MyNodeList<T> {
+  pub nodes: Vec<T>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct AllProposals {
+  pub proposals: MyNodeList<Proposal>,
+}
+
+pub fn deserialize_u64<'a, D>(d: D) -> Result<u64, D::Error>
+where
+	D: serde::Deserializer<'a>,
+{
+  let s: String = Deserialize::deserialize(d)?;
+  Ok(s.parse::<u64>().map_err(Error::custom)?)
+}
+
+pub fn deserialize_vec<'a, D>(d: D) -> Result<Vec<u64>, D::Error>
+where
+	D: serde::Deserializer<'a>,
+{
+  let s: String = Deserialize::deserialize(d)?;
+
+  Ok(s.split(',').map(|s| s.parse::<u64>().unwrap_or_default()).collect())
 }
